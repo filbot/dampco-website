1a98d57a987bad27fc7a39e6778bddcb56c7ac2b
Filip Grace <filip@filbot.com>
2024-04-23 10:49:41 -0700

    Add console log

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(
+    '%cDamp Co is a producer of fine products, ranging from garlic knots to sea sponges; at Damp Co, quality is paramount.',
+    'color: #800080; font-family: sans-serif; text-decoration: underline;'
+  );
